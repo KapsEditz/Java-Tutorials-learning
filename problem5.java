747173fda22b28f6995c6befd4e0be290d6d6c59
Kapil Chaudhari <chaudharikapil2708@gmail.com>
2026-06-24 23:44:45 +0530

    Add files via upload

---
+++ problem5.java
@@ -0,0 +1,22 @@
+package a1906;
+
+public class problem5 {
+
+	static int factorial(int n) {
+		if(n == 0 || n == 1) 
+			return 1;
+		
+		return n * factorial(n-1);
+		}
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		System.out.println("Factorial = " + factorial(num));
+	}
+
+}
+
+
+
+
